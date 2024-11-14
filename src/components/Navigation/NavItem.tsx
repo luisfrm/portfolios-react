4b4217e0065f08eb38fab3225fcaa0eaf04e2034
Luis Rivas <Luisfrm_@outlook.com>
2024-11-14 15:58:54 -0400

    Updated translations and improved the imports of these files

--- src/components/Navigation/NavItem.tsx
+++ src/components/Navigation/NavItem.tsx
@@ -5,9 +5,17 @@ interface Props {
 
 const NavItem = ({children, url}: Props) => {
 	const makeScrollTo = (hash: string) => {
-		console.log("Hello");
+		if (hash === "#") {
+			window.scrollTo({ top: 0, behavior: "smooth" });
+			return;
+		}
+		
 		const element = document.getElementById(hash);
-		// make 100px below the element
+		
+		if (!element) return;
+
+		// make 120px below the element
+		// 120px is the height of the fixed navbar
 		if (element) {
 			const y = element.getBoundingClientRect().top + window.scrollY - 120;
 			window.scrollTo({ top: y, behavior: "smooth" });
@@ -17,7 +25,8 @@ const NavItem = ({children, url}: Props) => {
 	return (
 		<div
 			onClick={()=>{makeScrollTo(url)}}
-			className="cursor-pointer border-transparent text-gray-700 dark:text-gray-400 hover:border-gray-300 hover:text-gray-700 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+			className="cursor-pointer border-transparent text-gray-900 dark:text-gray-400 hover:border-gray-300 hover:text-gray-500 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+			aria-label="Navigation Item"
 		>
 			{children}
 		</div>
