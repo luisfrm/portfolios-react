5892ccd1a60657fecac7437e542b67cf2bd00a5d
Luis Rivas <Luisfrm_@outlook.com>
2024-11-21 02:44:36 -0400

    updates on contact page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,6 @@
-import { Mail, MapPin, Phone } from "lucide-react";
+import { Download, Mail, MapPin, Phone } from "lucide-react";
+import { Button } from "../ui/button";
+import { goToPdf } from "@/lib/utils";
 
 interface Props {
 	content: {
@@ -6,6 +8,7 @@ interface Props {
 		email: string;
 		phone: string;
 		city: string;
+		download?: string;
 	};
 }
 
@@ -15,7 +18,11 @@ const Contact = ({ content }: Props) => {
 		<div id="contact" className="space-y-2">
 			{content.title && <h2 className="text-2xl font-semibold">{content.title}</h2>}
 			<div className="space-y-1">
-				<a href={`mailto:${email}`} className="flex items-center space-x-2 font-semibold" aria-label="Button that take you to send an email">
+				<a
+					href={`mailto:${email}`}
+					className="flex items-center space-x-2 font-semibold"
+					aria-label="Button that take you to send an email"
+				>
 					<Mail className="w-4 h-4" />
 					<span>{email}</span>
 				</a>
@@ -26,6 +33,11 @@ const Contact = ({ content }: Props) => {
 				<div className="flex items-center space-x-2 font-semibold">
 					<MapPin className="w-4 h-4" />
 					<span>{city}</span>
+				</div>
+				<div className="pt-5 flex justify-center md:justify-start">
+					<Button variant="outline" size="lg" onClick={goToPdf}>
+						<Download /> {content.download}
+					</Button>
 				</div>
 			</div>
 		</div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function goToPdf () {
+  const pdfURL = "https://drive.google.com/file/d/1t5MZ2PJZBJcREpQsmFFIOxVSnKuxKodF/view?usp=sharing"
+  window.open(pdfURL, "_blank")
+}
