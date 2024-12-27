da19d30122435ef3baf8e9798a0fef41d39534c5
Luis Rivas <Luisfrm_@outlook.com>
2024-12-27 01:39:44 -0400

    added snow animation

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,18 @@ import Contact from "./components/Contact/Contact";
 import SocialMedia from "./components/SocialMedia/SocialMedia";
 import Projects from "./components/Projects/Projects";
 import { es, en } from "@/translations";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocalStorage } from "usehooks-ts";
 import WorkExperience from "@/components/WorkExperienceAccordion/WorkExperience";
-
+import { generateSnow } from "@/lib/Snow";
 
 function App() {
 	const [lang, setLang] = useLocalStorage("lang", "en");
 	const [data, setData] = useState(lang === "en" ? en : es);
+
+	useEffect(() => {
+		generateSnow();
+	}, []);
 
 	const changeLanguage = () => {
 		setData(data === es ? en : es);
@@ -27,16 +31,16 @@ function App() {
 
 	return (
 		<ThemeProvider>
-			<div className="flex flex-col gap-16 mx-auto min-h-dvh bg-gradient-to-b from-slate-100 to-slate-300 dark:from-[#060436] dark:to-[#0f1236]">
+			<div className="flex flex-col gap-16 mx-auto min-h-dvh bg-gradient-to-b from-slate-200 to-slate-300 dark:from-[#060436] dark:to-[#0f1236]">
 				<Navigation
 					navItems={data.navItems}
 					changeLanguage={changeLanguage}
 					language={lang}
 					socialMedia={data.socialMedia}
 				/>
-				<Card className="w-full max-w-6xl mx-auto rounded-none sm:px-12 bg-transparent border-0 shadow-none py-36">
+				<Card className="w-full max-w-6xl mx-auto rounded-none sm:px-12 bg-transparent border-0 shadow-none pb-36">
 					<CardContent className="flex flex-col gap-16">
-						<section className="flex flex-col gap-4">
+						<section className="flex flex-col gap-4 h-screen lg:h-dvh justify-center">
 							<PersonalInfo content={data.personal} />
 							<Contact content={data.contact} />
 						</section>
